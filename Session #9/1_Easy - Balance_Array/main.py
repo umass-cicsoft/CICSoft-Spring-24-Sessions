37882ee7c067ed0fe50102ce028c510a46e081d7
Sagnik Pal <sagnikpal@umass.edu>
2024-04-29 19:08:54 -0400

    hackBits q1 main.py

---
+++ Session #9/1_Easy - Balance_Array/main.py
@@ -0,0 +1,39 @@
+"""
+Easy Problem: "Balanced Array"
+
+Problem Statement:
+    Given an integer array nums of even length, determine if it is possible to 
+    split the array into two equal length parts where the sum of the elements in 
+    the first half is equal to the sum of the elements in the second half. 
+    Return true  if such a split is possible, otherwise return false.
+
+Constraints:
+    2 <= nums.length <= 1000
+    nums.length is even.
+    -10^4 <= nums[i] <= 10^4
+
+Examples:
+    Example 1:
+        Input: nums = [1, 2, 3, 3]
+        Output: true
+        Explanation: The array can be split into [1, 2] and [3, 3], where both parts sum to 5.
+    Example 2:
+        Input: nums = [2, 3, 1, 2, 3, 1]
+        Output: true
+        Explanation: The array can be split into [2, 3, 1] and [2, 3, 1], where both parts sum to 6.
+    Example 3:
+        Input: nums = [1, 2, 1, 3]
+        Output: false
+        Explanation: There is no way to split the array into two parts with equal sums.
+"""
+
+input_list = []
+input_len = int(input())
+for _ in range(input_len):
+    input_list.append(int(input()))
+
+## TODO: Implement the balanced_array function below
+def balanced_array(input_list: list[int]) -> bool:
+    pass
+
+print(balanced_array(input_list))
